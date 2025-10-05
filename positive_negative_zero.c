c55e8af435bee6c788bee7cddef34cc916ee7710
Suhrid Singh <suhrids88@gmail.com>
2025-10-06 00:42:14 +0530

    Checks if the input integer is positive, negative or zero.

---
+++ positive_negative_zero.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+int a;
+  printf("Enter a number:\n");
+  scanf("%d",&a);
+  if (a>0){printf("The number is positive\n");}
+  else{if (a<0){printf("The number is negative\n");}
+      else{printf("The number is zero\n");}
+      }
+return 0;
+}
